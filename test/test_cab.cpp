b0a70fcc4e6c0038c6dacbaa8ad2c2ebbc0c5f58
Luigi Pannocchi <l.pannocchi@gmail.com>
2019-10-27 18:58:57 -0700

    Test program

--- test/test_cab.cpp
+++ test/test_cab.cpp
@@ -1,13 +1,26 @@
 #include <stdio.h>
 #include <opencv2/core/mat.hpp>
 #include "cab.hpp"
-
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 int main() {
 
-	cv::Mat E = cv::Mat::eye(10, 10, CV_8U);
+	cv::Mat E = 2 * cv::Mat::eye(10, 10, CV_8U);
 
+	std::cout << E << std::endl;
 	printf("Instantiate the CAB class\n");
 	CAB mycab(3);
+
+	printf("Insert element into the CAB\n");
+	mycab.insertData(E);
+
+	DataStorage* pDS = mycab.reserveRecordRD();
+
+	cv::Mat E_ = pDS->getData();
+	std::cout << E_ << std::endl;
+	cv::imshow("Fetched data", E_);
+	cv::waitKey(0);
 
 	return 0;
 }
